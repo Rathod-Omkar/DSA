3939412b8541e58c9e2f465e52208a4354b6b48c
Rathod-Omkar <rathodomkar007@gmail.com>
2025-07-09 18:20:40 +0000

    [LeetCode Sync] Runtime - 1 ms (88.78%), Memory - 54.4 MB (70.41%)

---
+++ Leetcode-Submissions/0001-two-sum/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+   let seen ={};
+    for(let i=0;i<nums.length;i++)
+    {
+        let diff = target - nums[i];
+        if(diff in seen)
+        {
+            return [seen[diff],i];
+            
+        }
+        else{
+            seen[nums[i]] = i;
+        }
+    }
+};
