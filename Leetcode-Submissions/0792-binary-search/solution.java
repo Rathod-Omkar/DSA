15d0823372b31a4b301b6fb6c7959dbec63eb28b
Rathod-Omkar <rathodomkar007@gmail.com>
2026-08-20 12:10:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 47.8 MB (96.51%)

---
+++ Leetcode-Submissions/0792-binary-search/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int binarySearch(int[] nums, int target,int low,int high){
+
+        if(low>high){
+            return -1;
+        }
+        int mid=(high+low)/2;
+         if(nums[mid]>target){
+            return binarySearch(nums,target,low,mid-1);
+        }
+        else if(nums[mid]<target){
+            return binarySearch(nums,target,mid+1,high);
+        }
+        return mid;
+    }
+    public int search(int[] nums, int target) {
+        return binarySearch(nums,target,0,nums.length - 1);
+    }
+}
