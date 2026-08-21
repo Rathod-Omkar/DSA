a64cd6df9da462144ea60fc65c59aea4d2f8efe3
Rathod-Omkar <rathodomkar007@gmail.com>
2026-08-21 17:56:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 80.2 MB (59.43%)

--- Leetcode-Submissions/0882-peak-index-in-a-mountain-array/solution.java
+++ Leetcode-Submissions/0882-peak-index-in-a-mountain-array/solution.java
@@ -1,7 +1,6 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         int low=0,high=arr.length-1,mid;
-        mid=low+(high-low)/2;
         while(low<high){
             mid=low+(high-low)/2;
             if(arr[mid]>arr[mid+1]){
