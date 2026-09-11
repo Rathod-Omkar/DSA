9f18ad6ac94a2fa4af12ab44586e4ac33b97f7d2
Rathod-Omkar <rathodomkar007@gmail.com>
2026-09-11 14:16:56 +0000

    [LeetCode Sync] Runtime - 47 ms (29.82%), Memory - 54.8 MB (5.73%)

---
+++ Leetcode-Submissions/2732-counter/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var num = null;
+var temp = 0;
+var createCounter = function(n) {
+    num = n-1;
+    
+    return function() {
+        num+=1;
+        return num;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
